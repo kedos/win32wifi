be0728e5cedcc216076e30fde22d54521fa57298
almondg <almond.snadbox@gmail.com>
2016-08-31 09:36:28 +0300

    Add stuff for pypi.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'win32wifi',
+  packages = ['win32wifi'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python Windows Wifi',
+  author = 'Shaked Gitelman',
+  author_email = 'shaked.dev@gmail.com',
+  url = 'https://github.com/almondg/win32wifi', # use the URL to the github repo
+  download_url = 'https://github.com/almondg/win32wifi/tarball/0.1', # I'll explain this in a second
+  keywords = ['windows', 'win32', 'wifi', 'wlan', 'wlanapi', 'wlanapi.dll'], # arbitrary keywords
+  classifiers = [],
+)
