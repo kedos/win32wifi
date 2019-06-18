fec65bc0dfdb882c58a6397da26d48d244df0c60
Oleg Kupreev <oleg.kupreev@gmail.com>
2019-06-19 01:42:19 +0300

    setup.py version changed

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md')) as f:
 setup(
     name='win32wifi',
     packages=['win32wifi'],  # this must be the same as the name above
-    version='0.1.0',
+    version='0.1.1',
     description='Python Windows Wifi - !Still Under Development!',
     long_description=long_description,
     author='Shaked Gitelman',
