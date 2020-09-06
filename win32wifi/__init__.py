27bd2a907cd7d7d02f0bdaeca66fbe65ad4f0452
kedos <almond.sandbox@gmail.com>
2020-09-06 14:56:30 +0300

    Update __init__.py (#9)

--- win32wifi/__init__.py
+++ win32wifi/__init__.py
@@ -1,2 +1,1 @@
-# from win32wifi.Win32NativeWifiApi import *
 from win32wifi.Win32Wifi import getWirelessAvailableNetworkList, getWirelessInterfaces, getWirelessNetworkBssList, getWirelessProfiles, getWirelessProfileXML, get_errno, get_last_error
